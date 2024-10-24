f2fe6bae3a121b9c46f0b2b5a9379eb7b2a1aab2
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2024-10-24 15:50:50 +0300

    Сделал так, чтобы при нажатии сочетания клавиш авто клик уходил в другой поток, а на первом потоке мы ловили события от винды

--- auto_click/auto_click.cpp
+++ auto_click/auto_click.cpp
@@ -1,49 +1,75 @@
 ﻿#include <windows.h>
 #include <iostream>
+#include <thread>
+#include <atomic>
 
-// Константы для сочетания клавиш Ctrl + Shift + A
 #define HOTKEY_ID 1 // ID горячей клавиши
+
+std::atomic<bool> flag{ false }; // Переменная для управления состоянием потока автоклика
 
 void auto_click()
 {
-    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); // Нажатие левой кнопки мыши
-    Sleep(1000); // Пауза в секунду
-    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); // Отпустили левую кнопку мыши
+    while (flag) // Пока флаг включен
+    {
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); // Нажатие левой кнопки мыши
+        Sleep(1000); // Пауза в 1 секунду
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); // Отпускание левой кнопки мыши
+        Sleep(1000); // Пауза в 1 секунду
+    }
 }
 
-bool check_hot_key(const MSG &msg, bool &flag)
+void start_auto_click()
 {
-    // msg.message == WM_HOTKEY. Здесь мы проверяем, относится ли событие к горячим клавишам
-    if (msg.message == WM_HOTKEY && msg.wParam == HOTKEY_ID) // Проверяем, соответствует ли нажатая кнопка HOTKEY_ID
-        return !flag;
+    flag = true; // Включаем флаг
+    std::thread t(auto_click); // Запускаем поток для автоклика
+    t.detach(); // Отсоединяем поток, чтобы он работал асинхронно
+}
 
-    return flag;
+void stop_auto_click()
+{
+    flag = false; // Останавливаем выполнение автоклика
+}
+
+bool check_hot_key(const MSG& msg)
+{
+    // Проверяем, является ли сообщение горячей клавишей
+    return (msg.message == WM_HOTKEY && msg.wParam == HOTKEY_ID);
 }
 
 int main()
 {
-    bool flag = false;
     setlocale(0, "");
 
-    if (RegisterHotKey(NULL, HOTKEY_ID, MOD_CONTROL | MOD_SHIFT, 0x41))
+    if (RegisterHotKey(NULL, HOTKEY_ID, MOD_CONTROL | MOD_SHIFT, 0x41)) // Ctrl + Shift + A
     {
-        std::cout << "Горячая клавиша зарегистрирована. Нажмите Ctrl + Shift + A для автонажатия мыши." << std::endl;
+        std::cout << "Горячая клавиша зарегистрирована. Нажмите Ctrl + Shift + A для автонажатия мыши.\n";
     }
     else
     {
-        std::cerr << "Не удалось зарегистрировать горячую клавишу." << std::endl;
+        std::cerr << "Не удалось зарегистрировать горячую клавишу.\n";
         return 1;
     }
 
     MSG msg = { 0 }; // Храним сообщения от Windows
-    while (GetMessage(&msg, NULL, 0, 0)) // Пока мы получаем какое-либо сообщение
+
+    while (GetMessage(&msg, NULL, 0, 0)) // Получаем сообщения от Windows
     {
-        while (check_hot_key(msg, flag))
+        if (check_hot_key(msg)) // Если горячая клавиша нажата
         {
-            auto_click(); 
+            if (!flag) // Если автоклик не запущен
+            {
+                std::cout << "Запуск автоклика...\n";
+                start_auto_click(); // Запускаем автоклик
+            }
+            else // Если автоклик уже запущен
+            {
+                std::cout << "Остановка автоклика...\n";
+                stop_auto_click(); // Останавливаем автоклик
+            }
         }
     }
 
+    // Освобождаем ресурсы после завершения программы
     UnregisterHotKey(NULL, HOTKEY_ID);
 
     return 0;
